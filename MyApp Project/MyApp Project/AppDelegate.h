eb3128e8b717fb544558ee09c70c22920724eef8
zhuchangwei <zhuchangwei@agliesc.com.cn>
2016-11-14 14:58:04 +0800

    新建工程

---
+++ MyApp Project/MyApp Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp Project
+//
+//  Created by asc-mac1 on 2016/11/14.
+//  Copyright © 2016年 Asc.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
